bb9a00399c5bcba42d94f97856adc1a20b8d56ef
Attila Maczak <attila@maczak.hu>
2017-10-30 14:37:04 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='ringbuffer',
+    version='0.1',
+    description='Ring buffer that allows for high-throughput data transfer'
+    'between multiproccessing Python processes.',
+    url='https://github.com/bslatkin/ringbuffer')
