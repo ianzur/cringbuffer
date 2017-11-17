e889558ed5b7e6958a56a8d96f9dcf9404896226
Attila Maczak <attila@maczak.hu>
2017-11-17 09:24:39 -0500

    add py_modules to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from distutils.core import setup
 
 setup(
     name='ringbuffer',
-    version='0.1',
+    version='0.2',
     description='Ring buffer that allows for high-throughput data transfer'
     'between multiproccessing Python processes.',
-    url='https://github.com/bslatkin/ringbuffer')
+    url='https://github.com/ctrl-labs/cringbuffer',
+    py_modules=['ringbuffer'])
