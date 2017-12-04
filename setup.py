f3b574a4c001f6671cd1467d973b501a534e353c
Nishant Kakar <nishant@cognescent.com>
2017-12-04 14:02:32 -0500

    update new version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='ringbuffer',
-    version='0.2',
+    version='0.3',
     description='Ring buffer that allows for high-throughput data transfer'
     'between multiproccessing Python processes.',
     url='https://github.com/ctrl-labs/cringbuffer',
